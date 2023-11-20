dcb1435fc9ce9e18ad5836fc96e2740789feb87a
Juan Manuel Conde Aldana <condejuan27@gmail.com>
2023-11-20 01:02:50 -0500

    update

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _dotenv = _interopRequireDefault(require("dotenv"));
-var _serverModel = _interopRequireDefault(require("./models/serverModel/server.model.js"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-_dotenv.default.config();
-const server = new _serverModel.default();
-server.listen();
